ea08f630bcfd6c20c4b2c692e146437305efbac1
DESKTOP-LGF9G09\rav <ravusz91@gmail.com>
2025-03-22 18:43:10 +0100

    feat(T-1) add translations

--- app/(tabs)/projects/[projectId]/tasks/components/TasksCarousel/TasksCarousel.tsx
+++ app/(tabs)/projects/[projectId]/tasks/components/TasksCarousel/TasksCarousel.tsx
@@ -2,6 +2,7 @@ import TaskCard from "../taskCard";
 
 import Button from "@/components/button";
 import { translate } from "@/i18n";
+import { TASK_STATUSES } from "../../constants";
 
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
@@ -11,23 +12,22 @@ import type { TaskResponse } from "../../api/api";
 type Props = {
   data: TaskResponse[];
 };
+
 const TasksCarousel = ({ data }: Props) => {
   return (
     <View style={styles.container}>
       <PagerView style={styles.container} initialPage={0}>
-        <View style={styles.page} key="1">
-          <Text>First page</Text>
-          <Text>Swipe ➡️</Text>
-          {data?.map((item) => {
-            return <TaskCard key={item.id} {...item} />;
-          })}
-        </View>
-        <View style={styles.page} key="2">
-          <Text>Second page</Text>
-        </View>
-        <View style={styles.page} key="3">
-          <Text>Third page</Text>
-        </View>
+        {Object.values(TASK_STATUSES).map((status) => {
+          return (
+            <View style={styles.page} key={status}>
+              <Text>{translate(`taskStatuses.${status}`)}</Text>
+              <Text>Swipe ➡️</Text>
+              {data?.map((item) => {
+                return <TaskCard key={item.id} {...item} />;
+              })}
+            </View>
+          );
+        })}
       </PagerView>
     </View>
   );
